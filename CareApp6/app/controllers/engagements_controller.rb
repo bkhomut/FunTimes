fa9dc378df7e527d43e9162599d643415a2e3510
Boris <bkhomut2013@kellogg.northwestern.edu>
2013-06-10 16:53:10 -0500

    Changed everything

--- CareApp6/app/controllers/engagements_controller.rb
+++ CareApp6/app/controllers/engagements_controller.rb
@@ -6,6 +6,9 @@ class EngagementsController < ApplicationController
 
   def show
     @engagement = Engagement.find_by_id(params[:id])
+    permission = Permission.where(:engagement_id => params[:id])
+    @permissions = Engagement.all && permission 
+
   end
 
   def new
@@ -16,12 +19,15 @@ class EngagementsController < ApplicationController
     @engagement = Engagement.new
     @engagement.user_id = params[:user_id]
     @engagement.condition_id = params[:condition_id]
-    
+
+    ##@user_name = User.find_by_id(:user_id)
+
     if @engagement.save
-      redirect_to engagements_url
+      redirect_to engagement_url(@engagement.id)
     else
       render 'new'
     end
+  
   end
 
   def edit
